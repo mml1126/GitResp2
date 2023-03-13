fbe70e901dd4c8526a63e239ac8ea2ace3428a29
mml1126 <mml1126@126.com>
2023-03-13 14:22:53 +0800

    增加了Person类

---
+++ TestModule01/src/com/msb/Person.java
@@ -0,0 +1,11 @@
+package com.msb;
+
+/**
+ * @author : mml
+ * @version : 1.0
+ * @date : 2023/3/13 - 03 - 13 - 14:21 
+ * @description : com.msb
+ */
+public class Person {
+    private int age;
+}
